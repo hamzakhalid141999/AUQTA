a0a93ac9b7fa8009093a0684b98ac7ecb31b94c4
Hamza141999 <hamza.khalid@stixor.com>
2022-04-09 13:14:13 +0500

    minor bugfixes

--- components/navbar/index.js
+++ components/navbar/index.js
@@ -1,14 +1,13 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import classes from "./navbar.module.css";
 import Image from "next/image";
 import logo from "../../public/assets/logo.png";
 import logo_black from "../../public/assets/logo_black.png";
-import {Link} from 'react-scroll'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import Link from "next/link";
 
 function Navbar() {
-
   const [categorySelected, setCategorySelected] = useState("buy");
   const [backgroundColor, setBackgroundColor] = useState("transparent");
   const [showFilter, setShowFilter] = useState(false);
@@ -16,15 +15,14 @@ function Navbar() {
 
   const handleCategorySelected = (value) => {
     setCategorySelected(value);
-  }
+  };
 
   useEffect(() => {
     document.addEventListener("scroll", () => {
       let scrolled = document.scrollingElement.scrollTop;
-      if (scrolled > 600){
+      if (scrolled > 600) {
         setShowFilter(true);
-      }
-      else{
+      } else {
         setShowFilter(false);
       }
       if (scrolled >= 70) {
@@ -43,71 +41,131 @@ function Navbar() {
   }, [backgroundColor]);
 
   return (
-    <div className={backgroundColor === "opaque" ? classes.navbar_body_opaque : classes.navbar_body}>
-      <div className={showFilter ? classes.filter_panel : classes.filter_panel_hidden}>
+    <div
+      className={
+        backgroundColor === "opaque"
+          ? classes.navbar_body_opaque
+          : classes.navbar_body
+      }
+    >
+      <div
+        className={
+          showFilter ? classes.filter_panel : classes.filter_panel_hidden
+        }
+      >
         <div className={classes.filter_panel_content}>
           <div className={classes.btns_container}>
-            <div onClick={()=>{
-              handleCategorySelected("buy");
-            }} className={classes.filter_panel_btn}>
-              <p className={categorySelected === "buy" ? classes.filter_panel_btn_label_selected : classes.filter_panel_btn_label_unselected}>BUY</p>
+            <div
+              onClick={() => {
+                handleCategorySelected("buy");
+              }}
+              className={classes.filter_panel_btn}
+            >
+              <p
+                className={
+                  categorySelected === "buy"
+                    ? classes.filter_panel_btn_label_selected
+                    : classes.filter_panel_btn_label_unselected
+                }
+              >
+                BUY
+              </p>
             </div>
-            <div className={classes.divider}/>
-            <div onClick={()=>{
-              handleCategorySelected("rent");
-            }} className={classes.filter_panel_btn}>
-              <p className={categorySelected === "rent" ? classes.filter_panel_btn_label_selected : classes.filter_panel_btn_label_unselected}>RENT</p>
+            <div className={classes.divider} />
+            <div
+              onClick={() => {
+                handleCategorySelected("rent");
+              }}
+              className={classes.filter_panel_btn}
+            >
+              <p
+                className={
+                  categorySelected === "rent"
+                    ? classes.filter_panel_btn_label_selected
+                    : classes.filter_panel_btn_label_unselected
+                }
+              >
+                RENT
+              </p>
             </div>
-            <div className={classes.divider}/>
-            <div onClick={()=>{
-              handleCategorySelected("invest");
-            }} className={classes.filter_panel_btn}>
-              <p className={categorySelected === "invest" ? classes.filter_panel_btn_label_selected : classes.filter_panel_btn_label_unselected}>INVEST</p>
+            <div className={classes.divider} />
+            <div
+              onClick={() => {
+                handleCategorySelected("invest");
+              }}
+              className={classes.filter_panel_btn}
+            >
+              <p
+                className={
+                  categorySelected === "invest"
+                    ? classes.filter_panel_btn_label_selected
+                    : classes.filter_panel_btn_label_unselected
+                }
+              >
+                INVEST
+              </p>
             </div>
           </div>
           <div className={classes.search_bar}>
-              <div className={classes.search_category}>
-                <p>CITY</p>
-                <h3>BALAKAN MOUNTAINS</h3>
-              </div>
-              <div className={classes.divider2} />
-              <div className={classes.search_category}>
-                <p>LOCATION</p>
-                <h3>SOUTH EASTERN EUROPE</h3>
-              </div>
-              <div className={classes.divider2} />
-              <div className={classes.search_category}>
-                <p>PROPERTY TYPE</p>
-                <h3>PRIVATE HOUSE</h3>
-              </div>
-              <div className={classes.divider2} />
-              <div className={classes.search_category}>
-                <p>PRICE RANGE</p>
-                <h3>$40000 - $60000</h3>
-              </div>
-              <div className={classes.search_icon}>
-                <FontAwesomeIcon
-                  className={classes.icon}
-                  icon={faSearch}
-                  size={"1x"}
-                />
-              </div>
+            <div className={classes.search_category}>
+              <p>CITY</p>
+              <h3>BALAKAN MOUNTAINS</h3>
             </div>
+            <div className={classes.divider2} />
+            <div className={classes.search_category}>
+              <p>LOCATION</p>
+              <h3>SOUTH EASTERN EUROPE</h3>
+            </div>
+            <div className={classes.divider2} />
+            <div className={classes.search_category}>
+              <p>PROPERTY TYPE</p>
+              <h3>PRIVATE HOUSE</h3>
+            </div>
+            <div className={classes.divider2} />
+            <div className={classes.search_category}>
+              <p>PRICE RANGE</p>
+              <h3>$40000 - $60000</h3>
+            </div>
+            <div className={classes.search_icon}>
+              <FontAwesomeIcon
+                className={classes.icon}
+                icon={faSearch}
+                size={"1x"}
+              />
+            </div>
+          </div>
         </div>
       </div>
       <div className={classes.left_panel}>
-        <div className={classes.logo}>
-          <Image src={backgroundColor === "transparent" ? logo : logo_black} alt="logo" />
-        </div>
+        <Link href={"/"}>
+          <div className={classes.logo}>
+            <Image
+              src={backgroundColor === "transparent" ? logo : logo_black}
+              alt="logo"
+            />
+          </div>
+        </Link>
       </div>
       <div className={classes.right_panel}>
-        <p><a href="/about">ABOUT</a></p>
+        <Link href={"/about"}>
+          <p>ABOUT</p>
+        </Link>
         <p>SERVICES</p>
-        <p><Link to="buy" spy={true} smooth={true}>BUY</Link></p>
-        <p><Link to="buy" spy={true} smooth={true}>SELL</Link></p>
-        <p><Link to="buy" spy={true} smooth={true}>RENT</Link></p>
-        <p><Link to="trending" spy={true} smooth={true}>INVEST</Link></p>
-        <p><a href="/signUp">LOGOUT</a></p>
+        <p>
+          <Link href={"#"}>BUY</Link>
+        </p>
+        <p>
+          <Link href={"#"}>SELL</Link>
+        </p>
+        <p>
+          <Link href={"#"}>RENT</Link>
+        </p>
+        <p>
+          <Link href={"#"}>INVEST</Link>
+        </p>
+        <Link href={"/signUp"}>
+          <p>LOGOUT</p>
+        </Link>
       </div>
     </div>
   );
