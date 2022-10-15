3d52d8c5f6fbb4955e4740c59c11751edfdcc62e
Hamza Khalid <hamza.khalid@stixor.com>
2022-10-15 18:04:37 +0500

    import error fixed

--- components/screenComponents/blogScreen/blogImage.js
+++ components/screenComponents/blogScreen/blogImage.js
@@ -1,4 +1,4 @@
-export const blog1Img = require("../../public/assets/blogs/blog1/img1.jpg");
+export const blog1Img = require("../../../public/assets/blogs/blog1/img1.jpg");
 
 export default {
   blog1Img,
